dc51ae854b0b17423fc63c5e10f21ba4e2e9f1f6
amardSha <amardeep.contact@gmail.com>
2026-09-10 12:54:18 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "./"
+// });
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./"
-});
+  plugins: [react()],
+  base: '/amarPortfolio/',
+})
